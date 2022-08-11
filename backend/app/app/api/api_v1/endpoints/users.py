34e63d2a10dc36df3f569bf84d97f5689906c2cf
guxal <jonathanacp93@gmail.com>
2022-08-10 20:13:19 -0500

    add user me update param

--- backend/app/app/api/api_v1/endpoints/users.py
+++ backend/app/app/api/api_v1/endpoints/users.py
@@ -130,6 +130,7 @@ def update_user_me(
     password: str = Body(None),
     full_name: str = Body(None),
     email: EmailStr = Body(None),
+    avatar: str = Body(None),
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
@@ -143,6 +144,8 @@ def update_user_me(
         user_in.full_name = full_name
     if email is not None:
         user_in.email = email
+    if avatar is not None:
+        user_in.avatar = avatar
     user = crud.user.update(db, db_obj=current_user, obj_in=user_in)
     return user
 
